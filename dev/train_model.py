705bc4ff57d63a651f28038bf23891dcecc78696
pjanthony2001 <104015990+pjanthony2001@users.noreply.github.com>
2024-07-06 12:59:18 +0800

    Update train model

--- dev/train_model.py
+++ dev/train_model.py
@@ -8,14 +8,21 @@ import re
 from sklearn.preprocessing import normalize
 from sklearn.model_selection import train_test_split
 
+from nltk.stem.porter import PorterStemmer
+
 REGEX = re.compile('[^a-zA-Z ]')
+KEYWORDS_PER_CATEGORY = 10
 CATEGORIES = ['fitness', 'athletics', 'training', 'running', 'gear', 'gym', 'exercise', 'sportswear', 'outdoors', 'wellness',
               'cuisine', 'gourmet', 'organic', 'recipes', 'delivery', 'dining', 'snacks', 'cooking', 'restaurants', 'healthy',
               'concerts', 'streaming', 'instruments', 'bands', 'festivals', 'songs', 'albums', 'lessons', 'dj', 'sound',
               'consoles', 'accessories', 'esports', 'multiplayer', 'virtual', 'pc', 'development', 'merchandise', 'streaming', 'communities',
               'streaming', 'shows', 'smart', 'reviews', 'theater', 'cable', 'series', 'reality', 'channels', 'binge']
 
-# why is smart in TV here?
+# why is smart in TV here? also rename Categories to keywords
+STEMMER = PorterStemmer()
+STEMMED_CATEGORIES = list(map(STEMMER.stem, CATEGORIES))
+
+print(f"Keywords is multiple of # of categories {CATEGORIES % KEYWORDS_PER_CATEGORY == 0}")
 
 def read_file_to_array(file_path):
     with open(file_path, 'r') as file:
@@ -25,12 +32,17 @@ def read_file_to_array(file_path):
 def process_text(text):
     global CATEGORIES
     global REGEX
-    text_alpha = REGEX.sub('', text)
-    text_lower = text_alpha.lower()
-    text_split = text_lower.split()
+    global STEMMER
     
-    freq = Counter(text_split)
-    vector = [freq[category] for category in CATEGORIES]
+    text = text.replace("-", " ")
+    text = REGEX.sub('', text)
+    text = text.lower()
+    text = text.split()
+    text = list(map(STEMMER.stem, text))
+    
+    
+    freq = Counter(text)
+    vector = [freq[category] for category in STEMMED_CATEGORIES]
     return vector
 
 file_path = 'training_data.txt'
@@ -53,12 +65,11 @@ normalised_data = normalize(corpus_mat, axis=1, norm='l1') #We can vary l1 or l2
 
 def non_linear_fn(x):
     # current model for testing
-    keywords_per_cat = 10
-    reshaped = x[:len(x) - len(x) % keywords_per_cat].reshape(-1, keywords_per_cat)
+    global KEYWORDS_PER_CATEGORY
+    print(f"SHAPE OF X is CORRECT {x.shape[0] % KEYWORDS_PER_CATEGORY == 0}")
+    reshaped = x[:len(x) - len(x) % KEYWORDS_PER_CATEGORY].reshape(-1, KEYWORDS_PER_CATEGORY)
     sums = np.sum(reshaped, axis=1)
     return sums
-
-print(non_linear_fn(np.arange(1, 50)))
 
 def get_training_data(X):
     y = np.apply_along_axis(non_linear_fn, axis=1)
@@ -81,4 +92,5 @@ params = {
 }
 
 concrete_classifier = NeuralNetRegressor(**params)
+get_training_data(X)
 
