5a13b91852ef2f605640dcd5a3f6d62f41b1be1d
vansh284 <vanshnath284@gmail.com>
2024-07-08 03:03:12 +0800

    Move logic from browser to client

--- search-engine/app/routes.py
+++ search-engine/app/routes.py
@@ -3,6 +3,8 @@ from app import app
 from .key import send_key_to_server_if_not_sent
 from .process_search_history import init_keywords, process_search_history, send_search_history_to_server
 import logging
+import random
+import requests
 
 logging.basicConfig(level=logging.INFO)
 
@@ -26,3 +28,29 @@ def send_search_history():
     logging.info("Successfully predicted")
     return jsonify(prediction), 200
 
+@app.route('/get_ads', methods = ['POST'])
+def send_ads():
+    good_ads = []
+    data = request.json.get('prediction')
+    best_ad = max(data, key=data.get)
+    ads = [best_ad]
+    remaining_ads = [ad for ad in data if ad not in ads]
+    next_best_ad = max(remaining_ads, key=lambda ad: data[ad] if data[ad] != data[best_ad] else float('-inf'))
+    ads.append(next_best_ad)
+    good_ads = list(ads)
+    logging.info("Successfully selected best ads")
+    remaining_ads = [ad for ad in remaining_ads if ad not in ads]
+    noisy_ad_1 = random.choice(remaining_ads)
+    ads.append(noisy_ad_1)
+    remaining_ads = [ad for ad in remaining_ads if ad not in ads]
+    noisy_ad_2 = random.choice(remaining_ads)
+    ads.append(noisy_ad_2)
+    logging.info("Successfully added noisy ads")
+    imageURLs = [f"http://server:5001/image/{cat}_1.jpg" for cat in ads]
+    for url in imageURLs:
+        try:
+            requests.get(url)
+        except requests.exceptions.RequestException as e:
+            logging.error("Error getting image: %s", e)
+    goodImageURLS = [f"http://localhost:5001/image/{cat}_1.jpg" for cat in good_ads]
+    return jsonify(goodImageURLS), 200
