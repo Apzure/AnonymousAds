45335cea6337fe080bd3b44a0e5cafee02a48e54
vansh284 <vanshnath284@gmail.com>
2024-07-08 02:09:07 +0800

    Clean code

--- search-engine/app/routes.py
+++ search-engine/app/routes.py
@@ -13,7 +13,6 @@ def index():
 
 @app.route('/send_search_history', methods=['POST'])
 def send_search_history():
-    # return jsonify({"food" : 1, "gaming" : 0.5, "music" : 0.2, "sports" : 0.1, "tv" : 0.25}), 200
     search_history = request.json.get('searchHistory')
     if not search_history:
         return jsonify({"error": "No search history provided"}), 400
@@ -22,6 +21,7 @@ def send_search_history():
     init_keywords()
     send_key_to_server_if_not_sent()
     search_history_vector = process_search_history(search_history) 
+    logging.info("Successfully processed search history")
     prediction = send_search_history_to_server(search_history_vector)
     logging.info("Successfully predicted")
     return jsonify(prediction), 200
