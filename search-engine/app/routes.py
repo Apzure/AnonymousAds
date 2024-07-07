3f416b35b30a01de00757a002b8135612f631201
vansh284 <vanshnath284@gmail.com>
2024-07-07 22:21:36 +0800

    Comment out temporary jsonifty

--- search-engine/app/routes.py
+++ search-engine/app/routes.py
@@ -13,7 +13,7 @@ def index():
 
 @app.route('/send_search_history', methods=['POST'])
 def send_search_history():
-    return jsonify({"food" : 1, "gaming" : 0.5, "music" : 0.2, "sports" : 0.1, "tv" : 0.25}), 200
+    # return jsonify({"food" : 1, "gaming" : 0.5, "music" : 0.2, "sports" : 0.1, "tv" : 0.25}), 200
     search_history = request.json.get('searchHistory')
     if not search_history:
         return jsonify({"error": "No search history provided"}), 400
